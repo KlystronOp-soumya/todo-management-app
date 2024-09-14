21edb1b9f348d5744ef1d064428dd3aa7690abbe
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-09-15 00:31:59 +0530

    Modifies App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ToDoApp from './components/ToDoApp'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
+      <Header />
       <ToDoApp />
+      <Footer />
     </div>
   );
 }
