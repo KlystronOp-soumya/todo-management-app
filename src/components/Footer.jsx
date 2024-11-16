44d71e0040e9c92c3d2b49b07257c7315bf39c91
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-11-16 20:56:35 +0530

    Adds authcontext into footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "../Context/AuthContext";
+
 const Footer = () => {
+
+  //add the Auth context here
+  const authContext  = useContext(AuthContext) ;
+  console.log(`Foother component - ${authContext.number}`) ;
+  
   return (
     <footer className="footer">
       <div className="container">
