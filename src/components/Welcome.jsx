53d1832b6666ce9a3da7c3c29fe903ae6a95c88b
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-11-16 20:57:03 +0530

    Puts the authcontext onto Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import { useParams , Link } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
+import { useAuth } from "../Authentication/AuthProvider";
 
 const Welcome = () =>{
 
     const {userName} = useParams() ; //to use the RequestParams
+    /*
+    //dont use the below code anymore
     const auth = useContext(AuthContext) ;
     console.log(auth.number)
+    */
+    //use the hook
+    const authContext = useAuth() ;
+    console.log(authContext.number) ;
     return(
         <div className="welcomeContainer">
             <h3>Welcome {userName}</h3>
