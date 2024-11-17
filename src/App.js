000ba7ec06b79d72929db34c84f53dcfe1759626
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-11-17 13:25:57 +0530

    removes unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import ToDoApp from './components/ToDoApp'
-import Header from './components/Header'
-import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
