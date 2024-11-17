d7322492ee44da4cb3cf0c7f6f9e6405222ac5b2
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-11-17 13:24:58 +0530

    Modifies AuthProviderto handle Login,logout

--- src/Authentication/AuthProvider.jsx
+++ src/Authentication/AuthProvider.jsx
@@ -1,26 +1,52 @@
-import { createContext, useContext, useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../Context/AuthContext";
 //import { AuthContext } from "../Context/AuthContext";
 
 // add the AuthContext and create a hook
 //1. Create context
-export const AuthContext = createContext() ;
+// export const AuthContext = createContext() ;
 //2.create a hook
 export const useAuth = () => useContext(AuthContext) ;
 
 const AuthProvider = ({children}) => {
 
     const [number , setNumber] = useState(1) ;
+
+    const [isAuthenticated , setAuthenticated] = useState(false) ;
     
-    //to check if the AuthContext is updating
+    //Deprecated
     function updateNumber()
     {
         setNumber(number+10) ;
     }
-
-    setInterval(updateNumber, 10000);
+    //function to handle the login
+    
+    const login = (username , password) => {
+        //logic extracted from Login component
+        if(username === '123' && password === '123')
+        {
+            setAuthenticated(true) ;
+            return true ;
+        }
+        else
+        {
+            setAuthenticated(false) ;
+            return false ;
+        }
+    }
+    //function to handle the logout
+    const logout = () => {
+        //logic extracted from Login component
+        setAuthenticated(false) ;
+    }
+    const securityContextHolder = {number , isAuthenticated , login , logout} ;
+    
     
     return(
-        <AuthContext.Provider value={ {number}  }>
+        /* 
+        <AuthContext.Provider value={ securityContextHolder  }>  
+        */
+        <AuthContext.Provider value={ securityContextHolder  }>
             {children} {/* Passesd down to the children */}
         </AuthContext.Provider>
     )
