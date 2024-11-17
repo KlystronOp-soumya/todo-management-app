14a32f8e410c32dfe8bf3a2a5ffc95c64309d617
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-11-17 14:06:37 +0530

    Adds function to Authenticate the routes

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,34 +1,63 @@
 import '../styles/ToDoApp.css'
-import Login from './Login' ;
-import Welcome from './Welcome' ;
+import Login from './Login';
+import Welcome from './Welcome';
 import Error from './Error';
 import ListTodo from './ListTodo';
 //Add the react router
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-const ToDoApp = ()=>{
+import { useAuth } from '../Authentication/AuthProvider';
 
-    return(
+const AuthenticatedRoute = ({ children }) => {
+    console.log("Authenticating the routes");
+    
+    const authContext = useAuth();
+    if (authContext.isAuthenticated)
+        return children;
+
+    return <Navigate to="/" />
+}
+
+const ToDoApp = () => {
+
+    return (
         <div className="toDoAppContainer">
             <label>ToDo Management Application</label>
             {/* <Login/>
             <Welcome/> */}
             <BrowserRouter>
-            <Header />{/* Header component */}
+                <Header />{/* Header component */}
                 <Routes>
-                    <Route path='/' element={ <Login /> } />
-                    <Route path='/login' element={ <Login /> } />
+                    <Route path='/' element={<Login />} />
+                    
+                    <Route path='/login' element={
+                    <AuthenticatedRoute>    
+                        <Login />
+                    </AuthenticatedRoute>
+                    } />
+
                     {/* Added named parameter */}
-                    <Route path='/welcome/:userName' element={ <Welcome/> } />
-                    <Route path='/todos' element={ <ListTodo />} />
-                    <Route path='/error' element={<Error/>} />
+                   
+                        <Route path='/welcome/:userName' element={
+                             <AuthenticatedRoute >
+                                <Welcome />
+                            </AuthenticatedRoute> 
+                        } />
+                   
+                    <Route path='/todos' element={
+                         <AuthenticatedRoute >
+                        <ListTodo />
+                        </AuthenticatedRoute>
+                        
+                        } />
+                    <Route path='/error' element={<Error />} />
                 </Routes>
-            <Footer/> {/* Footer component */}
+                <Footer /> {/* Footer component */}
             </BrowserRouter>
         </div>
     )
 
 }
 
-export default  ToDoApp ;
+export default ToDoApp;
